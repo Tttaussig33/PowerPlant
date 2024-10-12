966de7995be5a2b026d9a314c1d7f2bf2fd0a36d
Evan Lyons <150301463+EvanL77@users.noreply.github.com>
2024-10-12 12:13:43 -0400

    Added rcoks and AI spider
    added rocks and AI spider

--- PowerPlant/Assets/script/SpiderScript.cs
+++ PowerPlant/Assets/script/SpiderScript.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class SpiderScript : MonoBehaviour
 {
+    public Transform target;
+    public float speed = 3f;
+    private Rigidbody2D rb;
     public AudioClip _audioClip;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        GetTarget(); // Get the target immediately on start
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!target) GetTarget();
     }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            // Calculate the direction to the player
+            Vector2 direction = (target.position - transform.position).normalized;
+            // Update the velocity to move towards the player
+            rb.velocity = direction * speed;
+        }
+    }
+
+    private void GetTarget()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            target = player.transform;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(_audioClip, transform.position);
