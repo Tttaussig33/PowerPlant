91811f623b6d07222e5d3551fb9592ff04b57500
Evan Lyons <150301463+EvanL77@users.noreply.github.com>
2024-10-18 13:16:00 -0400

    Fonts
    Fonts

--- PowerPlant/Assets/script/HealthManager.cs
+++ PowerPlant/Assets/script/HealthManager.cs
@@ -2,56 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;  // Add this line for TextMeshPro
 
 
 public class HealthManager : MonoBehaviour
 {
-   public Image healthBar;
-   public float healthAmount = 100f;
+  public Image healthBar; 
+  public TMP_Text healthText;  // Use TMP_Text instead of Text if using TextMeshPro
+  public float healthAmount = 100f;
 
 
-   // Start is called before the first frame update
-   void Start()
-   {
-      
-   }
+  void Start()
+  {
+      UpdateHealthUI();
+  }
 
 
-   // Update is called once per frame
-   void Update()
-   {
-       if (healthAmount <= 0)
-       {
-           Application.LoadLevel(Application.loadedLevel);
-       }
+  void Update()
+  {
+      if (healthAmount <= 0)
+      {
+          Application.LoadLevel(Application.loadedLevel);
+      }
 
 
-       if (Input.GetKeyDown(KeyCode.Return))
-       {
-           TakeDamage(20);
-       }
+      if (Input.GetKeyDown(KeyCode.Return))
+      {
+          TakeDamage(20);
+      }
 
 
-       if (Input.GetKeyDown(KeyCode.Space))
-       {
-           Heal(5);
-       }
-   }
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+          Heal(5);
+      }
+  }
 
 
-   public void TakeDamage(float damage)
-   {
-       healthAmount -= damage;
-       healthBar.fillAmount = healthAmount / 100f;
-   }
+  public void TakeDamage(float damage)
+  {
+      healthAmount -= damage;
+      healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+      UpdateHealthUI();
+  }
 
 
-   public void Heal(float healingAmount)
-   {
-       healthAmount += healingAmount;
-       healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+  public void Heal(float healingAmount)
+  {
+      healthAmount += healingAmount;
+      healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+      UpdateHealthUI();
+  }
 
 
-       healthBar.fillAmount = healthAmount / 100f;
-   }
+  void UpdateHealthUI()
+  {
+      healthBar.fillAmount = healthAmount / 100f;
+      healthText.text = "HP: " + healthAmount.ToString("F0") + "%";  // Update the TMP text
+  }
 }
